ce0ff82a599a542090a8504f675141608024d92e
guerdani929 <GuerDani929@GoTVDSB.ca>
2020-11-09 13:38:53 -0500

    Finsihed

--- FinalGame/Game1.cs
+++ FinalGame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace FinalGame
 {
@@ -12,12 +13,20 @@ namespace FinalGame
         private Texture2D background;
         private Texture2D character;
         private KeyboardState oldState;
+        private Texture2D upwards;
+        private Texture2D sideways;
+        List<Rectangle> walls;
 
         
         Vector2 characterPosition;
         float characterSpeed;
 
+
+
         Screen currentLevel;
+
+       
+
 
 
         enum Screen
@@ -34,6 +43,7 @@ namespace FinalGame
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
+            
 
         }
 
@@ -44,9 +54,22 @@ namespace FinalGame
             characterPosition = new Vector2(graphics.PreferredBackBufferWidth / 2,
             graphics.PreferredBackBufferHeight / 2);
             characterSpeed = 220f;
+            walls = new List<Rectangle>();
 
-
+            walls.Add( new Rectangle(0, 0, 25, 480));
+            walls.Add( new Rectangle(0, 0, 665, 25));
+            walls.Add( new Rectangle(775, 40, 25, 450));
+            walls.Add( new Rectangle(165, 160, 25, 220));
+            walls.Add(new Rectangle(165, 140, 285, 25));
+            walls.Add(new Rectangle(620, 100, 25, 295));
             base.Initialize();
+
+            
+
+
+            
+           
+
         }
 
         protected override void LoadContent()
@@ -57,6 +80,11 @@ namespace FinalGame
 
             currentLevel = Screen.Intro;
             character = Content.Load<Texture2D>("PrisonnerB");
+
+            upwards = Content.Load<Texture2D>("UpWall");
+            sideways = Content.Load<Texture2D>("SideWall");
+            
+            
 
 
         }
@@ -82,7 +110,11 @@ namespace FinalGame
 
             if (kstate.IsKeyDown(Keys.Up))
                 characterPosition.Y -= characterSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
                 
+             
+
+
 
 
             if (kstate.IsKeyDown(Keys.Down))
@@ -91,21 +123,31 @@ namespace FinalGame
 
             if (kstate.IsKeyDown(Keys.Left))
                 characterPosition.X -= characterSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                
+
+                //foreach (Rectangle wall in walls)
+                //    if (characterPosition.Intersects(walls))
+                //    {
+                //        characterPosition.X = wall.Right;
+                //    }
+
+
+
 
             if (kstate.IsKeyDown(Keys.Right))
                 characterPosition.X += characterSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 
 
-            if (characterPosition.X > graphics.PreferredBackBufferWidth - character.Width) 
-                characterPosition.X = graphics.PreferredBackBufferWidth - character.Width;
-            else if (characterPosition.X < character.Width)
-                characterPosition.X = character.Width ;
 
-            if (characterPosition.Y > graphics.PreferredBackBufferHeight - character.Height)
-                characterPosition.Y = graphics.PreferredBackBufferHeight - character.Height;
-            else if (characterPosition.Y < character.Height)
-                characterPosition.Y = character.Height;
+            if (characterPosition.X > graphics.PreferredBackBufferWidth - character.Width/3) 
+                characterPosition.X = graphics.PreferredBackBufferWidth - character.Width/3;
+            else if (characterPosition.X < character.Width/3)
+                characterPosition.X = character.Width/3 ;
+
+            if (characterPosition.Y > graphics.PreferredBackBufferHeight - character.Height/3)
+                characterPosition.Y = graphics.PreferredBackBufferHeight - character.Height/3;
+            else if (characterPosition.Y < character.Height/3)
+                characterPosition.Y = character.Height/3;
+
 
             base.Update(gameTime);
         }
@@ -128,8 +170,9 @@ namespace FinalGame
             spriteBatch.Begin();
 
             spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
-
             
+
+
 
 
             spriteBatch.End();
@@ -158,14 +201,33 @@ namespace FinalGame
 
                 spriteBatch.Begin();
 
-
+                
                 spriteBatch.Draw(character, characterPosition, Color.White);
+                
+                //spriteBatch.Draw(upwards, new Rectangle(0, 0, 25, 480), Color.White);
+                //spriteBatch.Draw(sideways, new Rectangle(0, 0, 665, 25), Color.White);
+                //spriteBatch.Draw(upwards, new Rectangle(775, 40, 25, 450), Color.White);
 
+                ////Level Design
+                //spriteBatch.Draw(upwards, new Rectangle(165, 160, 25, 220), Color.White);
+                //spriteBatch.Draw(sideways, new Rectangle(165, 140, 285, 25), Color.White);
+                //spriteBatch.Draw(upwards, new Rectangle(620, 100, 25, 295), Color.White);
+
+                foreach (Rectangle wall in walls)
+                {
+                    if (wall.Width > wall.Height)
+                        spriteBatch.Draw(sideways, wall, Color.White);
+                    else
+                        spriteBatch.Draw(upwards, wall, Color.White);
+
+
+                }
 
                 spriteBatch.End();
 
+     
             }
-
+            //Finish game!!!
 
 
             base.Draw(gameTime);
